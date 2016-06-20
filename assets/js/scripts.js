3826fc9d9510e80e54e17baee5f3ee97e5922c3a
Tori Hedden <tori.hedden@gmail.com>
2016-06-20 16:14:41 -0400

    there's a small bug in tablet & mobile contact button logic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,4 +9,26 @@ $(document).ready(function(){
 
     $(".menu-wrap").toggle();
   });
+
+  $(".about-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projects").offset().top},
+        'slow');
+  });
+
+  $(".about-button2").click(function(){
+
+    if ($("#contact").css('display') !== 'none'){
+      $('html, body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+    } else {
+      $('html, body').animate({
+        scrollTop: $(document).height()},
+        'slow');
+      setInterval(function() { $(".menu-wrap").show() }, 600);
+    }
+
+  });
+
 });
