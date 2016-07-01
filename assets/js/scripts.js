55a0dbbcc06e79bc74a0623217f79ac858a24c5b
Tori Hedden <tori.hedden@gmail.com>
2016-07-01 13:26:48 -0400

    add some comments, wishing I had used bower components due to lack of wifi

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,19 @@
 
 $(document).ready(function(){
 
+//initialize visibility of the X to close the mobile view contact menu as hidden
   $(".fa-times").hide();
 
+//toggle between chat bubble and X on click floating round icon
   $(".float-icon").click(function(){
     $(".fa-comment-o").toggle();
     $(".fa-times").toggle();
 
+//toggle the mobile view contact menu visibility
     $(".menu-wrap").toggle();
   });
 
+//on click View my work, scroll page to projects section
   $(".about-button").click(function() {
     $('html, body').animate({
         scrollTop: $("#projects").offset().top},
@@ -28,7 +32,8 @@ $(document).ready(function(){
         'slow');
       setInterval(function() { $(".menu-wrap").show() }, 600);
     }
-
+//try changing .show() on line 33 to toggle... why is the menu toggling after line 23 event listener fires?
   });
 
 });
+//investigate media-query specific js files... instead of checking whether or not the desktop footer is shown, really the behavior we care about is tied to the media query. better befinition of when this should happen.
