aebf991982cc956be437fb4161bec42cf5c43934
Tori Hedden <tori.hedden@gmail.com>
2018-09-27 22:00:20 -0400

    begin refactor of js, css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,45 @@
+document.getElementById('copyright').innerHTML = "&copy;" + (new Date()).getFullYear() + " Tori Hedden";
 
-$(document).ready(function(){
-  
-$("#copyright").html("&copy; " + (new Date()).getFullYear() + " Tori Hedden");
+var closeMenu = document.getElementsByClassName('fa-times')[0];
+closeMenu.style.display = 'none';
+var openMenu = document.getElementsByClassName('fa-comment-o')[0];
+openMenu.style.display = 'inline-block';
+var mobileMenu = document.getElementsByClassName('menu-wrap')[0];
+var mobileMenuButton = document.getElementsByClassName('float-icon')[0];
 
-//initialize visibility of the X to close the mobile view contact menu as hidden
-  $(".fa-times").hide();
+mobileMenuButton.addEventListener('click', function() {
+  if (closeMenu.style.display === 'none') {
+    closeMenu.style.display = 'inline-block';
+    openMenu.style.display = 'none';
+    mobileMenu.style.display = 'block';
+  } else {
+    closeMenu.style.display = 'none';
+    openMenu.style.display = 'inline-block';
+    mobileMenu.style.display = 'none';
+  }
+})
 
-//toggle between chat bubble and X on click floating round icon
-  $(".float-icon").click(function(){
-    $(".fa-comment-o").toggle();
-    $(".fa-times").toggle();
+var projectSectionTop = document.getElementById('projects').getBoundingClientRect().top;
+document.getElementsByClassName('about-button')[0].addEventListener('click', function() {
+  window.scrollTo(0, projectSectionTop);
+})
 
-//toggle the mobile view contact menu visibility
-    $(".menu-wrap").toggle();
-  });
+//   $(".about-button2").click(function(){
+//     //basically, if in desktop media query, execute if. else is for tablet and phone
+//     if ($("#contact").css('display') !== 'none'){
+//       $('html, body').animate({
+//         scrollTop: $("#contact").offset().top},
+//         'slow');
+//     } else {
+//       $('html, body').animate({
+//         scrollTop: $(document).height()
+//       },'slow');
+//       // setInterval(function() {
+//         $(".menu-wrap").show();
+//         $(".fa-comment-o").hide();
+//         $(".fa-times").show();
+//       // }, 600);
+//     }//ok this fixes the issue but it doesn't look as good... try and re-work in a pause before the contact menu flies out that doesn't cause it to re-open and close
+//   });
 
-//on click View my work, scroll page to projects section
-  $(".about-button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#projects").offset().top},
-        'slow');
-  });
-
-  $(".about-button2").click(function(){
-    //basically, if in desktop media query, execute if. else is for tablet and phone
-    if ($("#contact").css('display') !== 'none'){
-      $('html, body').animate({
-        scrollTop: $("#contact").offset().top},
-        'slow');
-    } else {
-      $('html, body').animate({
-        scrollTop: $(document).height()
-      },'slow');
-      // setInterval(function() {
-        $(".menu-wrap").show();
-        $(".fa-comment-o").hide();
-        $(".fa-times").show();
-      // }, 600);
-    }//ok this fixes the issue but it doesn't look as good... try and re-work in a pause before the contact menu flies out that doesn't cause it to re-open and close
-  });
-
-});
-//investigate media-query specific js files... instead of checking whether or not the desktop footer is shown, really the behavior we care about is tied to the media query. better definition of when this should happen.
+// });
