b7b105397e137ec059d21dcd98dcf542e4511d1e
torihedden <tori.hedden@gmail.com>
2025-03-27 23:18:13 +0000

    refactor layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,6 @@ const work = document.getElementsByClassName("section-header")[0];
 const themeToggle = document.getElementById("toggle-switch");
 const siteThemeName = document.getElementById("site-theme-name");
 const favorites = document.getElementsByClassName("section-header")[2];
-const tagsWrapper = document.getElementsByClassName("tags-wrapper")[0];
 const topScrollButton = document.getElementById("top-scroll");
 
 const year = document.getElementById("year");
@@ -25,14 +24,10 @@ const isInViewport = (el) => {
     boundBox.top >= 0 &&
     boundBox.left >= 0 &&
     boundBox.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+    (window.innerHeight || document.documentElement.clientHeight) &&
     boundBox.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
+    (window.innerWidth || document.documentElement.clientWidth)
   );
-};
-
-const animateToolTags = () => {
-  tagsWrapper.classList.add("animate-tags");
 };
 
 themeToggle.addEventListener("change", () => {
@@ -46,15 +41,3 @@ work.addEventListener("click", () => {
 topScrollButton.addEventListener("click", () => {
   content.scrollIntoView({ behavior: "smooth" });
 });
-
-window.onscroll = () => {
-  if (isInViewport(tagsWrapper)) {
-    animateToolTags(tagsWrapper);
-  }
-
-  if (isInViewport(favorites)) {
-    topScrollButton.classList.remove("hide");
-  } else {
-    topScrollButton.classList.add("hide");
-  }
-};
